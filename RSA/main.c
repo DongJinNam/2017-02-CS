f0cc6c7e933252f045ed8e9ef630a3302cf25005
DESKTOP-8H727PG\NDJ <ehdwls6856@gmail.com>
2017-11-26 08:11:20 +0900

    RSA 최종 파일

--- RSA/main.c
+++ RSA/main.c
@@ -2,51 +2,51 @@
 #include <stdlib.h>
 #include "xxhash.h"
 
-typedef long long ll;
+typedef long long ll; // long long -> ll
 
-// square and multiply
+// square and multiply algorithm
 ll squareAndMultiply(ll a, ll p, ll n);
-// prime -> 1, composite -> -1
-ll milerRabinTest(ll n);
-// mod inverse using with extended euclid
+// 매개변수 n이 소수인지 아닌지 판별하는 함수.
+ll isPrime(ll n);
+// 소수인지 아닌지 판별하기 위해 miller-rabin primarlity test를 사용합니다.
+ll millerRabinTest(ll d, ll n);
+// mod inverse using with extended euclidean algorithm
 ll modInverse(ll a, ll n);
-// encryption RSA
-ll encrypt_RSA(ll m, ll e, ll n);
 
 int main() {
-	ll n, phi;
-	int i, j;
-	ll p, q, r, e, d;
+	ll n, phi; // (p*q*r), ((p-1)*(q-1)*(r-1))
+	int i, j; // for or while loop iterator
+	ll p, q, r, e, d; // 30-bit triple RSA(p,q,r) + 암호화 결과, 복호화 결과 (e,d)
 	ll hashed, hashed2, hashed3, sign;
-	ll message_in, message_out;
-	ll cipher;	
+	ll message_in, message_out; // 암호화 전 정수, 복호화 후 정수
+	ll cipher; // 암호화된 정수	
 	unsigned __int64 hash, hash2;
-	char buf[65], buf2[65];
-
-	srand(time(NULL));
+	char buf[65], buf2[65]; // 전자서명에서 사용할 버퍼.
 
 	//****************************************
-	//key setup
+	//key setup with miler-rabin test
 	//****************************************	
+	srand(time(NULL)); // random
 	p = rand() % 1024;
-	while (milerRabinTest(p) < 0) {
+	while (isPrime(p) < 0) {
 		p = rand() % 1024;
 	}
 	q = rand() % 1024;
-	while (milerRabinTest(q) < 0) {
+	while (isPrime(q) < 0) {
 		q = rand() % 1024;
 	}
 	r = rand() % 1024;
-	while (milerRabinTest(r) < 0) {
+	while (isPrime(r) < 0) {
 		r = rand() % 1024;
 	}
 	n = p*q*r;
 	phi = (p - 1)*(q - 1)*(r - 1);
-
+	// e - random prime
 	e = rand() % phi;
-	while (milerRabinTest(e) < 0) {
+	while (isPrime(e) < 0) {
 		e = rand() % phi;
 	}
+	// d - multiplicative inverse (extended euclidean algorithm 사용)
 	d = modInverse(e, phi);
 
 	printf("p = %lld\n", p);
@@ -58,7 +58,7 @@ int main() {
 	printf("d = %lld\n\n", d);
 
 	printf("Message Input : ");
-	scanf("%lld", &message_in);
+	scanf("%lld", &message_in); // 입력받은 정수 출력
 	printf("Message : %lld\n\n", message_in);
 
 	// Digital Signature Sign
@@ -66,31 +66,34 @@ int main() {
 	hash = XXH64(buf, sizeof(buf) - 1, 0);
 	hashed = hash % n;
 
+	// 전자서명 혹은 RSA 암호화/복호화 과정에서 square and multiply algorithm을 사용합니다.
 	printf("**Encryption\n");
-	cipher = encrypt_RSA(message_in, e, n);
-	sign = squareAndMultiply(hashed, d, n);
-	printf("cipher : %lld\n\n", cipher);
+	cipher = squareAndMultiply(message_in, e, n);
+	sign = squareAndMultiply(hashed, d, n); // 전자서명 값.
+	printf("cipher : %lld\n\n", cipher); // 암호화된 정수 출력
 
 	printf("**Generate signature\n");
-	printf("message's hash value : %d\n",hashed);
-	printf("generated signature : %d\n", sign);
+	printf("message's hash value : %lld\n",hashed); // 입력받은 정수 hash 함수 결과물
+	printf("generated signature : %lld\n", sign); // 전자서명 결과값
 	printf("\n\n");
 
-	message_out = encrypt_RSA(cipher, d, n);
+	message_out = squareAndMultiply(cipher, d, n); // 복호화 과정도 square and multiply algorithm을 사용한다.
+	// message_out += 1; (중간에서 메시지를 조작하기 위한 코드이기 때문에 주석처리를 하였습니다.)
 	printf("**Decryption\n");
 	printf("decrypted cipher : %lld\n\n", message_out);
 
 	// Digital Signature Verify
-	sprintf(buf2, "%lld", message_out);
-	hash2 = XXH64(buf2, sizeof(buf2) - 1, 0);
-	hashed2 = hash2 % n;
-	hashed3 = squareAndMultiply(sign, e, n);
+	sprintf(buf2, "%lld", message_out); // 복호화 과정을 거친 정수 출력
+	hash2 = XXH64(buf2, sizeof(buf2) - 1, 0); // 복호화 결과 정수로 hash 함수 적용
+	hashed2 = hash2 % n; // compare value with hashed3
+	hashed3 = squareAndMultiply(sign, e, n); // compare value with hashed2
 
 	printf("**Verify signature\n");
-	printf("received signature value : %d\n",sign);
-	printf("decrypted message's hash value : %d\n", hashed2);
-	printf("verify value from signature : %d\n", hashed3);
+	printf("received signature value : %lld\n",sign);
+	printf("decrypted message's hash value : %lld\n", hashed2);
+	printf("verify value from signature : %lld\n", hashed3);
 
+	// hashed2 와 hashed3 이 같아야 전자서명이 valid하다.
 	if (hashed2 == hashed3)
 		printf("Signature valid!\n");
 	else
@@ -102,43 +105,59 @@ ll squareAndMultiply(ll a, ll p, ll n) {
 	int binary[64] = { 0 };
 	int idx = 0;
 	int i;
-	ll val = 1;
-	while (p > 0) {
+	ll val = 1; // 초기값은 1로 설정
+	while (p > 0) { // p값을 이진수로 변환
 		binary[idx++] = p % 2;
 		p /= 2;
 	}
 	for (i = idx - 1; i >= 0; i--) {
-		val = ((val % n) * (val % n)) % n;
-		if (binary[i] == 1) {
-			val = ((val % n) * (a % n)) % n;
+		val = ((val % n) * (val % n)) % n; // bit 값이 1,0 모두 square 연산을 진행한다.
+		if (binary[i] == 1) { 
+			val = ((val % n) * (a % n)) % n; // bit 값이 1인 경우, 기존 결과에 a를 한번 곱한다.
 		}		
 	}	
 	return val;
 }
 
-ll milerRabinTest(ll n) {
-	ll cmp = n - 1;
-	ll m, k, t;
-	ll count = 0;
-	ll a;
-	int i;	
-	
-	while (cmp % 2 == 0) {
-		cmp /= 2;
-		count++;
+// 매개변수 값이 소수인지 아닌지 판별하는 함수.
+ll isPrime(ll n)
+{
+	if (n <= 1 || n == 4) return -1;
+	if (n <= 3) return 1;
+
+	ll d = n - 1;
+	while (d % 2 == 0)
+		d /= 2;
+
+	for (int i = 0; i < 20; i++)
+		if (millerRabinTest(d, n) == -1)
+			return -1; // -1 : 합성수
+	return 1; // 1 : 소수
+}
+
+ll millerRabinTest(ll d, ll n) {
+	// 2 ~ n-2 범위에 있는 임의의 숫자 선택
+	ll a = 2 + rand() % (n - 4);
+
+	// a^d % n
+	ll x = squareAndMultiply(a, d, n);
+
+	if (x == 1 || x == n - 1)
+		return 1; // 소수
+
+	while (d != n - 1)
+	{
+		x = (x * x) % n;
+		d *= 2;
+		if (x == 1) return -1; // -1 : 합성수
+		if (x == n - 1) return 1; // 1 : 소수
 	}
-	m = cmp;
-	k = count;
-	for (i = 0; i < 20; i++) {
-		a = rand() % (n - 1);
-		t = squareAndMultiply(a, (1 << i) * m, n);
-		if (t % n == n - 1)
-			return 1;
-	}
+	// -1 : 합성수
 	return -1;
 }
 
 ll modInverse(ll a, ll n) {
+	// Extended Euclidean Algorithm을 사용하였습니다.
 	ll a1 = 1, a2 = 0, a3 = n;
 	ll b1 = 0, b2 = 1, b3 = a;
 	ll t1, t2, t3;
@@ -168,8 +187,3 @@ ll modInverse(ll a, ll n) {
 	}
 	return rtn;
 }
-
-ll encrypt_RSA(ll m, ll e, ll n) {
-	return squareAndMultiply(m, e, n);
-}
-
